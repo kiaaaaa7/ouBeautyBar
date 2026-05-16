e0dd98b8c91be73406cce417d00867fa8dc3ab87
zaskia <zaskiamaesa94187@gmail.com>
2026-05-16 14:51:35 +0700

    Tambah desain katalog dan seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +12,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('designs')->insert([
+            [
+                'nama' => 'Nail Art Pink',
+                'harga' => 50000,
+                'image' => 'design1.jpg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+            [
+                'nama' => 'Soft Glam Makeup',
+                'harga' => 120000,
+                'image' => 'design2.jpg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
     }
 }
